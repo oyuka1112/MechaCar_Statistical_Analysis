6435f7e68a528c97c689ad2ed62a5499aafae7d6
Oyun-Erdene Adilbish <64121596+oyuka1112@users.noreply.github.com>
2022-02-09 11:16:43 -0500

    del 1, 2, 4

---
+++ MechaCarChallange.RScript.R
@@ -0,0 +1,20 @@
+library(readr)
+### Deliverable 1
+MechaCar_mpg <- read_csv("MechaCar_mpg.csv") #read the data
+## Multiple Regression
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
+
+## Linear regression
+summary(lm(mpg ~ vehicle_weight, data = MechaCar_mpg))
+
+### Deliverable 2
+Suspension_Coil <- read_csv("Suspension_Coil.csv")
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median = median(PSI), Varience = var(PSI), SD = sd(PSI),) 
+total_summary 
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median = median(PSI), Varience = var(PSI), SD = sd(PSI),) 
+lot_summary
+
+### Deliverable 3
+
